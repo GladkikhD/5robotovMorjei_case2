3189e3334265030b5f90ef8e447ff38f8065a76c
pnu-2020102741 <2020102741@pnu.edu.ru>
2022-05-28 13:41:52 +1000

    Added neuronic net module

---
+++ _5robotovMorjei.py
@@ -0,0 +1,17 @@
+from ipaddress import NetmaskValueError
+
+
+class Net:
+    def __init__(self):
+        pass
+
+    def forward():
+        pass
+
+    def train():
+        pass
+
+    def findWay(x, y, destx, desty):
+        pass
+    
+
